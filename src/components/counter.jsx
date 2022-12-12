bf29a5d27636a93cce773f2530248e65a4875903
sampath <sampathr@hsenidmobile.com>
2022-12-12 05:42:41 +0530

    Add Counter app component

---
+++ src/components/counter.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {  } 
+    render() { 
+        return <h1>Hello Reat world</h1>;
+    }
+}
+ 
+export default Counter;
