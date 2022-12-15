668ac96a0bd932f57182c4c40a1859b3865722f9
sampath <sampathr@hsenidmobile.com>
2022-12-15 08:23:41 +0530

    Add increment and decrement buttons

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {  } 
-    render() { 
-        return <h1>Hello Reat world</h1>;
+  //binding this with a method
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //     console.log("Counter was created", this);
+  //   }
+  state = {
+    count: 0,
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Increment
+        </button>
+        <button
+          onClick={this.handleDecrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Decrement
+        </button>
+      </div>
+    );
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    if (this.state.count > 0) {
+      this.setState({ count: this.state.count - 1 });
     }
+  };
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+
+    classes += this.state.count <= 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    let { count } = this.state;
+    return count === 0 ? "zero" : count;
+  }
 }
- 
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Counter from './components/counter';
+import Counter from "./components/counter";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Counter/>
+    <Counter />
   </React.StrictMode>
 );
 
