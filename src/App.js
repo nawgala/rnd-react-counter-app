84f1996062ebf5b05f713b7c63b83efccf755f97
sampath <sampathr@hsenidmobile.com>
2022-12-25 14:43:17 +0530

    Add class to app component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 
 import NavBar from "./components/navbar";
 import Counters from "./components/counters";
 
 import "./App.css";
 
-function App() {
-  return (
-    <React.Fragment>
-      <NavBar />
-      <main className="container">
-        <Counters />
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Counters />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
