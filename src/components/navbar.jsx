b5a0318a4cb1f7689f0b89085ad9083ddee63b74
sampath <sampathr@hsenidmobile.com>
2022-12-26 05:45:29 +0530

    Add Stateless Functional Component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-          <span className="badge badge-pill badge-info m-2">
-            {this.props.totalCount}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+
+//Stateless Functional Component
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar
+        <span className="badge badge-pill badge-info m-2">
+          {this.props.totalCount}
+        </span>
+      </a>
+    </nav>
+  );
+};
+// class NavBar extends Component {
+//   render() {
+//     return (
+//       <nav className="navbar navbar-light bg-light">
+//         <a className="navbar-brand" href="#">
+//           Navbar
+//           <span className="badge badge-pill badge-info m-2">
+//             {this.props.totalCount}
+//           </span>
+//         </a>
+//       </nav>
+//     );
+//   }
+// }
 
 export default NavBar;
