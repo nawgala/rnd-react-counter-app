6f650bca17aecb2e18a168f590d35625752376fd
sampath <sampathr@hsenidmobile.com>
2022-12-26 05:54:26 +0530

    Add props to Sfc

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 
 //Stateless Functional Component
-const NavBar = () => {
+const NavBar = (props) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar
         <span className="badge badge-pill badge-info m-2">
-          {this.props.totalCount}
+          {props.totalCount}
         </span>
       </a>
     </nav>
